69422fefbe71fbb188f07e4a9b86d04f6877b661
h-latta <h.latta@yahoo.com>
2022-12-19 17:27:04 -0600

    Updated Bootstrap Import to Index, Removed Header from App

--- random-react-app/src/App.js
+++ random-react-app/src/App.js
@@ -3,7 +3,6 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <header>RelationFix</header>
       <h1>Hello world!</h1>
       <body>Welcome to RelationFix. Please select an option for the solution to your problems.</body>
     </div>
